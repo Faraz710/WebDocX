d73d4eb049e0b269814fefd6521f3b36acaae0f0
Faraz710 <faraz.suhail2000@gmail.com>
2020-09-08 20:50:56 +0530

    Added Error Page

---
+++ models/consultation_schema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const consultationSchema = new Schema({
+	problem: {
+		issue: {
+			type: String
+		},
+		tags: {
+			type:[String]
+		}
+	},
+	symptoms: {
+		type: [String]
+	},
+	description: {
+		type: String
+	},
+	doctorId: {
+		type: Schema.Types.ObjectId
+	},
+	patientId: {
+		type: Schema.Types.ObjectId
+	},
+	messages: [{
+		message: {
+			type: String
+		},
+		time: {
+			type: Date
+		}
+	}],
+	solved: {
+		type: Boolean,
+		default: false
+	},
+	publish: {
+		type: Boolean,
+		default: false
+	}
+});
+
+module.exports = Consultation = mongoose.model("Consultations", consultationSchema);
