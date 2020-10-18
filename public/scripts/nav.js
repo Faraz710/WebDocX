0662390b94dc759e4554c1e487786d16bf21ef1b
Harsh Vardhan <harsh.vardhan2019@vitstudent.ac.in>
2020-10-19 00:47:39 +0530

    notification working

---
+++ public/scripts/nav.js
@@ -0,0 +1,16 @@
+function docdrop(){
+    if(document.getElementById('drop').style.display=='none'){
+        document.getElementById('drop').style.display = "block";
+    }
+    else{
+        document.getElementById('drop').style.display = "none";
+    }
+  }
+  function notdrop(){
+    if(document.getElementById('unotification').style.display=='none'){
+        document.getElementById('unotification').style.display = "block";
+    }
+    else{
+        document.getElementById('unotification').style.display = "none";
+    }
+  }
--- public/scripts/viewdocs.js
+++ public/scripts/viewdocs.js
@@ -57,12 +57,5 @@ function scrollFunction() {
       }
 }
 
-function docdrop(){
-  if(document.getElementById('drop').style.display=='none'){
-      document.getElementById('drop').style.display = "block";
-  }
-  else{
-      document.getElementById('drop').style.display = "none";
-  }
-}
+
 
